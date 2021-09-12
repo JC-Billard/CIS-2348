1e32ba8885d448a1e6aa1603d4ef5bb5c7703cd8
JC-Billard <77754674+JC-Billard@users.noreply.github.com>
2021-09-12 13:26:03 -0500

    Delete Homework 3 directory

--- Homework 3/11.18.py
+++
@@ -1,9 +0,0 @@
-# Jake Billard UHID 1582534
-
-numeral_input = map(int, input().split())  #List Input
-
-number_list = [x for x in numeral_input if x >= 0]   #Removes the Negative Values
-
-number_list.sort()  #Sorts List from Smallest to largest
-
-print(*number_list, sep = " ", end='' + ' ')
